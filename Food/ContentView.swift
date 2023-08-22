54a72d2dbb55ed48a35931fed94fafb24934bdd1
myshine112 <bluesatellite777@gmail.com>
2023-08-23 07:49:08 +0800

    init

---
+++ Food/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food
+//
+//  Created by Justin Padilla on 7/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            OnBoarding()
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Justin Padilla on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
